7d69485922ca3f6b8c55b5f1cb100eb4b9c056c0
sdac-19 <sdac-19@sdac-19-PC>
2024-01-16 09:22:58 +0530

    update 1.4

--- Demo/src/Main2.java
+++ Demo/src/Main2.java
@@ -4,5 +4,6 @@ public static void main(String[] args) {
 	System.out.println("Information Technology");
 	System.out.println("Physics");
 	System.out.println("Chemistry");
+	System.out.println("Biology");
 }
 }
